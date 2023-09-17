e1cbfac040ae6b4a0ad81c9c912b45cf8b83644b
Shubham <shubhamdhaundiyal789@gmail.com>
2023-09-18 00:08:28 +0530

    Home Section Added

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <section className='home' id="home" >
+        <div className="content">
+            <h3>Fresh<span> Food in the</span> Morning</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, ab. Lorem ipsum dolor sit.</p>
+            <a href="#" className="btn">Get Yours Now</a>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home
